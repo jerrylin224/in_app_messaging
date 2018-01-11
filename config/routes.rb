5d41d041eda1455278e4abf322a3fff5f42ec0af
Jiahung Lin <pk60905@hotmail.com>
2018-01-11 16:14:16 +0800

    Add reply message feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :conversations, only: [:index, :show, :destroy]
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
+    end
+  end
 
   resources :messages, only: [:new, :create]
 
