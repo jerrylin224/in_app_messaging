26e26bff7a263b9335d9bd1513b0c1a69c4e35ac
Jiahung Lin <pk60905@hotmail.com>
2018-02-01 10:49:58 +0800

    Show selected image in real time before uploading it

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,17 +1,17 @@
-// $(document).ready(function() {
-//   function readURL(input) {
-//     if (input.files && input.files[0]) {
-//       var reader = new FileReader();
+$(document).ready(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
 
-//       reader.onload = function(e) {
-//         $('#image').attr('src', e.target.result);
-//       }
+      reader.onload = function(e) {
+        $('#image').attr('src', e.target.result);
+      }
 
-//       reader.readAsDataURL(input.files[0]);
-//     }
-//   }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
 
-//   $("#gallery_image").change(function() {
-//     readURL(this);
-//   });
-// });
+  $("#gallery_image").change(function() {
+    readURL(this);
+  });
+});
