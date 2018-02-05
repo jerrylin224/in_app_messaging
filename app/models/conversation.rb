590824c70bd0a6ff2c725b945894b09ad55d5b4d
Jiahung Lin <pk60905@hotmail.com>
2018-02-05 15:56:39 +0800

    Fix read conversation cant be trashed and sync with trashed inbox conversation issue

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,13 @@
 class Conversation < Mailboxer::Conversation
+  # Make the own scope to make read box effect
   scope :read, lambda {|participant|
-    participant(participant).merge(Mailboxer::Receipt.is_read)
+    participant(participant).merge(Mailboxer::Receipt.is_read.not_trash.not_deleted)
+  }  
+
+  # override the default seeting to make unread conversation be able to move to trash
+  # and move unread conversation to trash if same message is trashed in inbox
+  scope :unread, lambda {|participant|
+    participant(participant).merge(Mailboxer::Receipt.is_unread.not_trash.not_deleted)
   }  
 
   def sender_name
